0268674c74f69d5b5fdcaaaa8dd7be24100cf0a5
celocisneros <celo.cisneros@icloud.com>
2025-08-22 22:06:02 +0000

    made another change to serve js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const HOST = '0.0.0.0'; // listen on all network interfaces
 
 // Middleware to parse JSON (important if you want POST requests)
 app.use(express.json());
@@ -14,6 +15,6 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express API!' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
