7d8d6b9f5f1ede7b73249ffc707f9ca32bd6a6a1
celocisneros <celo.cisneros@icloud.com>
2025-08-31 17:03:04 +0000

    fuck it all

--- app.js
+++ app.js
@@ -166,25 +166,6 @@ app.get("/api/current-user", (req, res) => {
   }
 });
 
-//ROUTE TO UPDATE PIC
-app.post("/api/update-profile-pic-url", async (req, res) => {
-  if (!req.session || !req.session.player) {
-    return res.status(401).json({ error: "Not logged in" });
-  }
-
-  const playerId = req.session.player._id;
-  const { profilePicUrl } = req.body;
-
-  const updatedPlayer = await Player.findByIdAndUpdate(
-    playerId,
-    { profilePicUrl },
-    { new: true }
-  );
-
-  req.session.player = updatedPlayer; // keep session in sync
-  res.json({ player: updatedPlayer });
-});
-
 
 
 
