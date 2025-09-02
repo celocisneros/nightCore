e7a9cb77a544934670895fae60681aa1257ab375
celocisneros <celo.cisneros@icloud.com>
2025-09-01 19:47:21 -0500

    gonna try and load a monster yolo

--- public/javascript/monsterLogic.js
+++ public/javascript/monsterLogic.js
@@ -5,17 +5,17 @@ async function loadMonster(monsterName = "") {
       const res = await fetch(`/api/monsters/${monsterName}`);
       const monster = await res.json();
 
-      const container = document.getElementById("monster-container");
+      const container = document.getElementById("enemyStats");
       container.innerHTML = `
-        <h3>${monster.name}</h3>
+        <p>${monster.name}<p>
         <p>HP: ${monster.hp}</p>
         <p>Attack: ${monster.attack}</p>
-        <img src="${monster.imageUrl}" alt="${monster.name}" width="150">
+        <img src="${monster.profilePicPath}" alt="${monster.name}" width="150">
       `;
     } catch (err) {
       console.error("Error loading monster:", err);
     }
   }
 
-  // Load random monster when battle starts
-  loadMonster();
+  // Load random monster with no name in parameters
+  loadMonster(bat);
